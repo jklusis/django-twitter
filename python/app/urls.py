24954deeda2e2d1c9c0cb45216da967c0d35069b
Jānis Kristaps Lūsis <jklusis@outlook.com>
2020-01-05 22:52:00 +0200

    Webpack, Vue initialized

--- python/app/urls.py
+++ python/app/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from app import views as base_views
@@ -35,4 +37,5 @@ urlpatterns = [
     path('settings/delete-account', user_views.delete_account, name='delete-account'),
 
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ python/resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+import Vue from 'vue';
+
+const app = new Vue({
+    el: '#app',
+});
+
+export default app;
